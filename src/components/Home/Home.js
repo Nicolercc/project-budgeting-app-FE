0ce1f5642dfc9e720f4dd8381d95b4e5ef512b61
Nicole Rodriguez <nicolerodriguez@Nicoles-Air.lan>
2023-07-11 05:01:09 -0400

    loading page added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./Home.css";
 
 function Home({ transactionsArray }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   function formatDate(dateString) {
     const options = { month: "long", day: "numeric" };
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", options);
+  }
+
+  if (isLoading) {
+    return (
+      <div className="container">
+        <div className="loading-container">
+          <h2>Loading...</h2>
+          {/* You can add a loading spinner or animation here */}
+        </div>
+      </div>
+    );
   }
 
   return (
